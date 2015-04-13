d2bd8d63b5b558ba7c89e6d7ef2c26900a114ae4
Qinsi Long <longqinsi@gmail.com>
2015-04-13 17:16:02 +0800

    nuget package update
    Update nuget package version to 1.1.4

--- JsonConfig/AssemblyInfo.cs
+++ JsonConfig/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("Timo Dörr")]
+[assembly: AssemblyCopyright("Timo Dörr 2012-2015, Qinsi Long 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -17,7 +17,7 @@ using System.Runtime.CompilerServices;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.4.0")]
 
 // The following attributes are used to specify the signing key for the assembly, 
 // if desired. See the Mono documentation for more information about signing.
